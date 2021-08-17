b5512b6e3139fb99bc94130259db2163ee44aee7
BasharAlZawahreh <zawahrehbashar@gmail.com>
2021-08-17 22:28:34 +0300

    P P1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const axios = require("axios");
-
+const { triggerAsyncId } = require("async_hooks");
+/*
 const readFile = () => {
     console.log('Hi')
     fs.readFile("data.txt", (err,data) => {
@@ -47,3 +48,58 @@ const getPostAsync = async(id) => {
 };
 getPostAsync(1);
 getPostAsync(50);
+
+
+*/
+
+console.log('**************Practice*****************')
+console.log('**************Practice*****************')
+console.log('**************Practice*****************')
+/*
+console.log('P Q1')
+
+const fsPromises = fs.promises;
+const appendToFile = (data) => {
+    fsPromises.appendFile('data.txt',data, ()=>{
+        console.log("Data has been appended \n");
+    })
+};
+
+appendToFile('This text is appended!');
+*/
+
+/*
+console.log('P Q2')
+
+const copyFile = (fileName) => {
+    fs.copyFile(fileName, `copy_of_${fileName}`, ()=> {
+        console.log('done')
+    });
+};
+  
+copyFile('data.txt');
+*/
+
+
+console.log('P Q3')
+const post = {
+    title: "JavaScript Basics",
+    body: "This post contains information about javaScript ",
+    // the id of the user who is going to create the post
+    userId: 1,
+};
+
+const createPost = (post) => {
+    axios.post("https://jsonplaceholder.typicode.com/posts",post)
+    .then((res)=>{
+        console.log(res)
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+};
+
+createPost(post);
+
+
+
